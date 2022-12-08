e7d3e4b25b97b2a2fc3e418e72b66d048cfd8939
William Phan <williamphan@MacBook-Pro-40.fios-router.home>
2022-12-07 19:18:59 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,54 +1,16 @@
-/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
-	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
-				Rubik: ['Rubik', 'Sans-Serif'],
 				Roboto: ['Roboto Mono', 'monospace'],
-			},
-			keyframes: {
-				flash: {
-					'25%, 40%': { opacity: '10' },
-					'50%': { opacity: '20' },
-					'75%': { opacity: '0' },
-				},
-				flash1: {
-					'35%, 50%': { opacity: '10' },
-					'50%': { opacity: '20' },
-					'75%': { opacity: '0' },
-				},
-				flash2: {
-					'45%, 60%': { opacity: '10' },
-					'50%': { opacity: '20' },
-					'75%': { opacity: '0' },
-				},
-				flash3: {
-					'55%, 70%': { opacity: '10' },
-					'50%': { opacity: '20' },
-					'75%': { opacity: '0' },
-				},
-				flash4: {
-					'65%, 80%': { opacity: '10' },
-					'50%': { opacity: '20' },
-					'75%': { opacity: '0' },
-				},
-				flash5: {
-					'75%, 90%': { opacity: '10' },
-					'50%': { opacity: '20' },
-					'75%': { opacity: '0' },
-				},
+				Inter: ['Inter', 'sans-serif'],
 			},
 			animation: {
-				flash: 'flash 2s infinite',
-				flash1: 'flash1 2s infinite',
-				flash2: 'flash2 2s infinite',
-				flash3: 'flash3 2s infinite',
-				flash4: 'flash4 2s infinite',
-				flash5: 'flash5 2s infinite',
+				'spin-slow': 'spin 3s linear infinite',
 			},
 		},
 	},
-	plugins: [],
+	plugins: ['tailwindcss/nesting', 'postcss-nesting'],
 }
