6c58ffdce052ac326e4726e98b47eae5311b5ca6
William Phan <williamphan@MacBook-Pro-40.fios-router.home>
2022-12-14 18:15:53 -0500

    mint base

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -10,6 +10,7 @@ import {
 	usePrepareContractWrite,
 	useAccount,
 	useContractRead,
+	useWaitForTransaction,
 } from 'wagmi'
 import { ethers } from 'ethers'
 import LineChart from 'src/components/Charts/LineChart.js'
@@ -17,20 +18,49 @@ import React from 'react'
 import abi from 'src/abi/particle.json'
 import ReactIs from 'react-is'
 import RandomParticles from '@/components/RandomParticles'
+import { write } from 'fs'
 
 const Discover = () => {
 	const [string, setString] = React.useState<any>('')
 	const [image, setImage] = React.useState<any>('')
+
+	const { address, isConnecting, isDisconnected } = useAccount()
 
 	const getImage = useContractRead({
 		address: '0x7b076ddBA57042d3021A2d529531497580937250',
 		abi: abi,
 		functionName: 'getImage',
 		args: [string],
+
 		onSuccess(data) {
 			setImage(data)
 			console.log('Success', data)
 		},
+	})
+
+	const {
+		config,
+		error: prepareError,
+		isError: isPrepareError,
+	} = usePrepareContractWrite({
+		address: '0x7b076ddBA57042d3021A2d529531497580937250',
+		abi: [
+			{
+				inputs: [{ internalType: 'string', name: '_signal', type: 'string' }],
+				name: 'mint',
+				outputs: [{ internalType: 'uint256', name: 'id', type: 'uint256' }],
+				stateMutability: 'payable',
+				type: 'function',
+			},
+		],
+		functionName: 'mint',
+		args: [string],
+		enabled: Boolean(string),
+	})
+	const { data, error, isError, write } = useContractWrite(config)
+
+	const { isLoading, isSuccess } = useWaitForTransaction({
+		hash: data?.hash,
 	})
 
 	return (
@@ -41,8 +71,14 @@ const Discover = () => {
 			<section className="bg-black min-h-screen overflow-hidden flex flex-col pt-24 sm:flex-row sm:pt-0 items-center justify-center content-center sm:space-x-10 ">
 				{/* DISCOVER HERE */}
 				{/* w-10/12 sm:w-5/12 md:w-4/12 lg:w-5/12 xl:w-4/12 */}
-				<div className="flex flex-col items-center justify-center mb-28 w-10/12 sm:w-5/12 md:w-4/12 lg:w-5/12 xl:w-4/12  ">
-					{image ? <Image src={image} alt="getImage" height={350} width={400} /> : <></>}
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						write?.()
+					}}
+					className="flex flex-col items-center justify-center mb-28 w-10/12 sm:w-5/12 md:w-4/12 lg:w-5/12 xl:w-4/12  "
+				>
+					{image ? <Image src={image} alt="getImage" height={380} width={400} /> : <></>}
 					<h1 className="text-2xl py-3 font-Mont font-bold text-center">RENDER YOUR PARTICLE</h1>
 					<textarea
 						onChange={e => setString(e.target.value)}
@@ -50,8 +86,23 @@ const Discover = () => {
 						// value={collection}
 						placeholder="   ENTER SIGNAL..."
 					></textarea>
-					<button className=" border-2 rounded-xl font-Mont font-bold py-3 w-10/12">MINT ≈ 1.23 ETH</button>
-				</div>
+					{/* <button className=" border-2 rounded-xl font-Mont font-bold py-3 w-10/12">MINT ≈ 1.23 ETH</button> */}
+					<button
+						className=" border-2 rounded-xl font-Mont font-bold py-3 w-10/12"
+						disabled={!write || isLoading}
+					>
+						{isLoading ? 'MINTING...' : 'MINT ≈ 1.23 ETH'}
+					</button>
+					{isSuccess && (
+						<div>
+							Successfully minted your NFT!
+							<div>
+								<a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+							</div>
+						</div>
+					)}
+					{isError && <div>Error: {(prepareError || error)?.message}</div>}
+				</form>
 				<div className="w-10/12 sm:w-5/12 md:w-4/12 lg:w-5/12 xl:w-4/12 xl:mt-[-5.5rem]  ">
 					<LineChart />
 				</div>
