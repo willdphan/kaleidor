c57b9def65b06ec8e2a9581cdfdebdf6a35cb965
William Phan <williamphan@MacBook-Pro-40.fios-router.home>
2022-12-14 16:58:15 -0500

    render particle

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react'
+import { FC, useState, useEffect, ChangeEvent } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Header from '@/components/Header'
@@ -9,11 +9,28 @@ import {
 	useContractWrite,
 	usePrepareContractWrite,
 	useAccount,
+	useContractRead,
 } from 'wagmi'
 import { ethers } from 'ethers'
 import LineChart from 'src/components/Charts/LineChart.js'
+import React from 'react'
+import abi from 'src/abi/particle.json'
 
-const discover = () => {
+const Discover = () => {
+	const [string, setString] = React.useState<any>('')
+	const [image, setImage] = React.useState<any>('')
+
+	const getImage = useContractRead({
+		address: '0x7b076ddBA57042d3021A2d529531497580937250',
+		abi: abi,
+		functionName: 'getImage',
+		args: [string],
+		onSuccess(data) {
+			setImage(data)
+			console.log('Success', data)
+		},
+	})
+
 	return (
 		<>
 			<div>
@@ -22,12 +39,14 @@ const discover = () => {
 			<section className="bg-black min-h-screen overflow-hidden flex flex-col pt-24 sm:flex-row sm:pt-0 items-center justify-center content-center sm:space-x-10 ">
 				{/* DISCOVER HERE */}
 				{/* w-10/12 sm:w-5/12 md:w-4/12 lg:w-5/12 xl:w-4/12 */}
-				<div className="flex flex-col items-center justify-center mb-28 w-10/12 sm:w-5/12 md:w-4/12 lg:w-5/12 xl:w-4/12 border-2 py-10 rounded-xl">
+				<div className="flex flex-col items-center justify-center mb-28 w-10/12 sm:w-5/12 md:w-4/12 lg:w-5/12 xl:w-4/12  ">
 					<div>
-						<Image src={particle} alt="Picture" height={350} width={400} />
+						{/* height={350} width={400} */}
+						<Image src={image} alt="getImage" height={350} width={350} />
 					</div>
-					<h1 className="text-2xl py-3 font-Mont font-bold">RENDER YOUR PARTICLE</h1>
+					<h1 className="text-2xl py-3 font-Mont font-bold text-center">RENDER YOUR PARTICLE</h1>
 					<textarea
+						onChange={e => setString(e.target.value)}
 						className=" pb-24 w-10/12 text-white bg-black mt-4 border-2 placeholder:italic text-wrap mb-5 rounded-xl font-Mont"
 						// value={collection}
 						placeholder="   ENTER SIGNAL..."
@@ -42,4 +61,4 @@ const discover = () => {
 	)
 }
 
-export default discover
+export default Discover
